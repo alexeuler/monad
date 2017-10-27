7db4cafa86dc161706d4c81be22c3265da5fe28f
AlexeyKarasev <karasev.alexey@gmail.com>
2017-10-27 22:49:23 +0300

    some tweaks

--- python/src/__main__.py
+++ python/src/__main__.py
@@ -6,6 +6,7 @@ from future import Future
 from either import Either, Left, Right
 
 conn = http.client.HTTPSConnection("en.wikipedia.org")
+sema = threading.BoundedSemaphore(1)
 
 def read_file_sync(uri, cb):
   base_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
@@ -14,7 +15,7 @@ def read_file_sync(uri, cb):
     with open(path) as f:
       data = f.read()
     cb(Right(data))
-  except OSError as err:
+  except Exception as err:
     cb(Left(err))
 
 def read_file(uri):
@@ -24,12 +25,12 @@ def read_file(uri):
   return Future(read_async)
 
 def fetch_sync(uri, cb):
-  conn.request("GET", uri)
   try:
+    conn.request("GET", uri)
     r = conn.getresponse()
-    data = r.read()
+    data = r.read().decode("utf-8")
     cb(Right(data))
-  except OSError as err:
+  except Exception as err:
     cb(Left(err))
 
 def fetch(uri):
@@ -38,12 +39,15 @@ def fetch(uri):
     t.start()
   return Future(fetch_async)
 
+def fetch_short(uri):
+  return fetch(uri).map(lambda res: res[:200])
 
 def main(args=None):
   lines = read_file("../resources/urls.txt").map(lambda res: res.splitlines())
-  content = lines.flat_map(lambda urls: Future.traverse(urls)(fetch))
-  content.map(lambda res: print(res))
-  time.sleep(5)
+  content = lines.flat_map(lambda urls: Future.traverse(urls)(fetch_short))
+  output = content.map(lambda res: print("\n".join(res)))
+  output.map(lambda res: sema.release())
+  sema.acquire()
 
 if __name__ == "__main__":
   main()
