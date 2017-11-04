171b5c5d80608ccde1fe9c2de453ba07e7cabbd1
AlexeyKarasev <karasev.alexey@gmail.com>
2017-11-04 07:18:09 +0300

    added either

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -3,4 +3,6 @@ import monad._
 object Main extends App {
   println(MySome(1).map(_ + 2))
   println(MySome(1).flatMap(_ => MyNone))
+  println(MyLeft(1).flatMap(_ => MyRight(2)))
+  println(MyRight(1).map(_ + 10))
 }
--- scala/src/main/scala/monad/MyEither.scala
+++ scala/src/main/scala/monad/MyEither.scala
@@ -3,6 +3,9 @@ package monad
 sealed trait MyEither[+E, +A] {
   def flatMap[EE >: E, B](f: A => MyEither[EE, B])(implicit M: Monad[MyEither[EE, ?]]): MyEither[EE, B] =
     M.flatMap(this)(f)
+
+  def map[EE >: E, B](f: A => B)(implicit M: Monad[MyEither[EE, ?]]): MyEither[EE, B] =
+    M.map(this)(f)
   
 }
 case class MyLeft[E](e: E) extends MyEither[E, Nothing]
